a9df3b548439eadd5286d11e1638b610636ee550
ssie-2025 <ssie000014@gmail.com>
2025-08-14 16:54:55 +0530

    add some minor changes

---
+++ src/Components/GetTouch/GetTouch.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './GetTouch.css'
+import side_img from '../../assets/contact-img.png'
+
+function GetTouch() {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "9e0e4ef3-73a6-426b-b562-0373e90b2896");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+
+
+  return (
+    <div className="get-contact-section">
+      <div className="form-container">
+        <h2>📞 Get in Touch with PolicyMandi</h2>
+        <p>Your insurance journey starts here. Talk to our experts today.</p>
+        <form className="contact-form" onSubmit={onSubmit}>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Phone Number" />
+          <input type="text" placeholder="Subject" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+         <span> {result}</span>
+      </div>
+      <div className="image-container">
+        <img src={side_img} alt="Get in touch" />
+      </div>
+    </div>
+  )
+}
+
+export default GetTouch
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,17 +3,17 @@ import './Home.css'
 import Hero from "../../Components/Hero/Hero";
 import Heading from '../../Components/Heading/Heading'
 import Feature from '../../Components/Feature/Feature'
-import Video from '../../Components/Video/Video'
 import Insurancematter from '../../Components/InsuranceMatter/Insurancematter'
 import Whatweserve from '../../Components/Whatweserve/Whatweserve'
 import Bannersection from '../../Components/Bannersection/Bannersection'
 import Brandcarousel from '../../Components/BrandCarousel/Brandcarousel'
 import Whylifeinsurance from '../../Components/WhyLifeInsurance/Whylifeinsurance'
-import Quickaccess from '../../Components/Quickaccess/Quickaccess';
 import Scrolltopbtn from '../../Components/Scrolltopbtn/Scrolltopbtn';
 import Chatbot from '../../Components/Chatbot/Chatbot';
 import Testimonial from '../../Components/Testimonial/Testimonial';
 import TabCalculator from '../../Components/TabCalculator/TabCalculator';
+import GetTouch from '../../Components/GetTouch/GetTouch';
+import Quickaccess from '../../Components/Quickaccess/Quickaccess'
 
 
 
@@ -25,13 +25,14 @@ function Home() {
       <Scrolltopbtn/>
       <Heading  title='Why Choose PolicyMandi?'/>
       <Feature/>
-      {/* <Video/> */}
       <Whatweserve/>
       <TabCalculator/>
+      <Quickaccess/>
       <Insurancematter/> 
       <Heading  title='Our Parters' subTitle="Secure your future with leading insurance partners"/>
       <Brandcarousel/>
       <Bannersection/>
+      <GetTouch/>
       <Whylifeinsurance/>
       <Testimonial/>
     </>
